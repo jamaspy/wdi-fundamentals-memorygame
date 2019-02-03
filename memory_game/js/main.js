b5689bcfebd2ad8d60e148a0a180908ec6fc464a
Kylie Clark <kylieclark@Kylies-iMac.lan>
2019-02-03 16:51:38 +1100

    added in the javascript file

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+
+console.log("User flipped over" + " " + cardOne);
+console.log("User flipped over" + " " + cardTwo);
