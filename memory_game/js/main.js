6a3d309c3e2bbb25e81428c9d35e1cc9c3d59dcc
James Aspinall <jamesaspinall@Jamess-MacBook.local>
2019-02-06 11:25:56 +1100

    added start of javascript logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,20 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "king", "queen", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+if(cardsInPlay.length === 2){
+	if (cardsInPlay[0] === cardsInPlay[1]){
+	alert("You Found A Match!")
+	}else{
+	alert("Sorry, Try Again")
+	}
+}
+
+
 
 console.log("User flipped over" + " " + cardOne);
 console.log("User flipped over" + " " + cardTwo);
