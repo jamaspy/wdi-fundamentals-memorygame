d53d63b10c239eecef9d50a99b5a44dfe971c155
James Aspinall <jamesaspinall@Jamess-MacBook.local>
2019-02-11 15:06:53 +1100

    Final commit; added reset button and tinkered with the style

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,70 +1,60 @@
-
-var cards = [
-	{
+var cards = [{
 		rank: "queen",
 		suit: "hearts",
 		cardImage: "images/queen-of-hearts.png",
 	},
 	{
-	   rank: "queen",
-	   suit: "diamonds",
-	   cardImage: "images/queen-of-diamonds.png",
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png",
 	},
 	{
-	 rank: "king",
-	 suit: "hearts",
-	 cardImage: "images/king-of-hearts.png",
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png",
 	},
-	 {
-	 rank: "king",
-	 suit: "diamonds",
-	 cardImage: "images/king-of-diamonds.png",
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png",
 	},
- ];
- 
+];
+
 var cardsInPlay = [];
 var result = document.getElementById("result");
+var resetButton = document.getElementById("reset");
 
-  function checkForMatch() {
-	 if (cardsInPlay[0] === cardsInPlay[1]) {
-		   result.innerHTML = "YES, You Got It!!!";
-	 } else {
-	 	   result.innerHTML = "No Match :( Try Again."
-		 };
-   };
- 
- 
- function flipCard() {
-   var cardId = this.getAttribute("data-id");
-    cardsInPlay.push(cards[cardId].rank);
-    this.setAttribute("src", cards[cardId].cardImage);
-    if (cardsInPlay.length === 2){
-	 checkForMatch();
-    };
- };
- 
- 
-  function createBoard() {
-   var board = document.getElementById("game-board");
-   for (var i = 0; i < cards.length; i++) {
-   var cardElement = document.createElement("img");
-	 cardElement.setAttribute("src", "images/back.png");
-	 cardElement.setAttribute("data-id", i);
-	 cardElement.addEventListener("click", flipCard);
-	 board.appendChild(cardElement);
-   }
- };
- 
- createBoard();
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		result.innerHTML = "YES, You Got It!";
+	} else {
+		result.innerHTML = "No Match :( <br> Hit 'Play Again' for another go"
+	};
+};
+
+function flipCard() {
+	var cardId = this.getAttribute("data-id");
+	cardsInPlay.push(cards[cardId].rank);
+	this.setAttribute("src", cards[cardId].cardImage);
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	};
+};
+
+function createBoard() {
+	var board = document.getElementById("game-board");
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		board.appendChild(cardElement);
+	}
+};
+
+function playAgain() {
+	location.reload();
+}
+resetButton.addEventListener("click", playAgain);
+
+createBoard();
